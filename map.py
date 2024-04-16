35555a83dc5a6fe8305b0849e44fa6bde0cd8cae
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-04-16 22:10:15 +0200

    Update map.py Print char by char

--- map.py
+++ map.py
@@ -1,16 +1,24 @@
 import os
 
 
-def draw_map(map_width, map_height):
+def draw_map(map_width, map_height, char_to_print):
     os.system("clear")
 
     print("+--" + "---" * (map_width - 2) + "--+")
 
-    for i in range(map_height):
-        print("|  " + "   " * (map_width - 2) + "  |")
+    draw_char(map_width, map_height, char_to_print)
 
     print("+--" + "---" * (map_width - 2) + "--+")
 
 
+def draw_char(map_width, map_height, char_to_print):
+    for i in range(map_height - 2):
+        print("|  ", end="")
+        for j in range(map_width - 2):
+            print(" " + char_to_print + " ", end="")
+
+        print("  |")
+
+
 if __name__ == '__main__':
-    draw_map(21, 21)
+    draw_map(21, 23, " ")
