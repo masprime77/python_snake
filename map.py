653f129e923c50e37ecdf1ed8b15c53427635edf
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-04-16 22:21:18 +0200

    Update head.py Nos can print the rows with the position of the head
    Update map.py
    Move draw_char-function to head and implemented it

--- map.py
+++ map.py
@@ -1,24 +1,16 @@
 import os
 
 
-def draw_map(map_width, map_height, char_to_print):
+def draw_map(map_width, map_height, head):
     os.system("clear")
 
     print("+--" + "---" * (map_width - 2) + "--+")
 
-    draw_char(map_width, map_height, char_to_print)
+    head_pos = (0, 0)
+
+    head.draw_head(map_width, map_height, head, head_pos)
 
     print("+--" + "---" * (map_width - 2) + "--+")
 
 
-def draw_char(map_width, map_height, char_to_print):
-    for i in range(map_height - 2):
-        print("|  ", end="")
-        for j in range(map_width - 2):
-            print(" " + char_to_print + " ", end="")
 
-        print("  |")
-
-
-if __name__ == '__main__':
-    draw_map(21, 23, " ")
