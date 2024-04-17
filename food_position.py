7af6e575cdb558bbd657c7ed3bb5bece41c1cad5
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-04-17 16:04:43 +0200

    The food appears random at each step FIXED

--- food_position.py
+++ food_position.py
@@ -6,7 +6,4 @@ def food_position(map_width, map_height, food_pos, head_pos):
     while new_food_position == head_pos:
         new_food_position = [random.randint(0, map_height - 1), random.randint(0, map_width - 1)]
 
-    print(new_food_position)
-    print(range(map_width - 2))
-
     return new_food_position
