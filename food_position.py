90b23e851418ba4ba2347001da699c51e192b123
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-04-17 15:54:46 +0200

    The food appears random at each step

--- food_position.py
+++ food_position.py
@@ -1,2 +1,9 @@
-def food_position(food_pos):
-    return [10, 10]
+import random
+
+
+def food_position(map_width, map_height, food_pos, head_pos):
+    new_food_position = [random.randint(0, map_height - 2), random.randint(0, map_width - 2)]
+    while new_food_position == head_pos:
+        new_food_position = [random.randint(0, map_height - 2), random.randint(0, map_width - 2)]
+
+    return new_food_position
