f31a218f34e6fa7adf70e9d6e3d382595b48a520
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-04-17 20:34:26 +0200

    Extract initial_screen.py to another file for further use

---
+++ initial_screen.py
@@ -0,0 +1,9 @@
+import draw_row
+
+
+def initial_screen(map_width, map_height, head_pos, food_pos):
+    print("+" + "---" * map_width + "+")
+
+    draw_row.draw_row(map_width, map_height, "X", head_pos, "*", food_pos)
+
+    print("+" + "---" * map_width + "+")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import draw_row
 import draw_map
+import initial_screen
 
 
 def main():
@@ -8,11 +9,7 @@ def main():
     head_pos = [0, 0]
     food_pos = [10, 10]
 
-    print("+" + "---" * map_width + "+")
-
-    draw_row.draw_row(map_width, map_height, "X", head_pos, "*", food_pos)
-
-    print("+" + "---" * map_width + "+")
+    initial_screen.initial_screen(map_width, map_height, head_pos, food_pos)
 
     while True:
         draw_map.draw_map(map_width, map_height, head_pos, food_pos)
