8db36ef4fa1bfa223062c4ef0a09bc21e18b144b
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-05-14 09:48:36 +0200

    FIX. Food doesn't appear in the tail, but al limit it broke. Doesn't escape while loop.

--- draw_map.py
+++ draw_map.py
@@ -18,13 +18,15 @@ def draw_map(map_width, map_height, prev_head_pos, food_location, points, tail_l
     FOOD_CNT = 10
 
     while len(food_location) != FOOD_CNT:
-        food_location.append(food_position.food_positions(map_width, map_height, new_head_pos, food_location))
+        food_location.append(food_position.food_positions(map_width, map_height, new_head_pos, food_location,
+                                                          tail_location))
 
     score.score(points)
 
     print("+" + "---" * map_width + "+")
 
-    draw_row.draw_row(map_width, map_height, "X", new_head_pos, "*", food_location, ".", "$", tail_location)
+    draw_row.draw_row(map_width, map_height, "X", new_head_pos, "*", food_location, ".", "$",
+                      tail_location)
 
     print("+" + "---" * map_width + "+")
 
