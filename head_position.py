2692a9bef44274bfaa64d047649a825d3be750ca
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-05-14 12:03:00 +0200

    Added arrow mobility

--- head_position.py
+++ head_position.py
@@ -11,16 +11,16 @@ def head_position(map_width, map_height, head_pos):
 
         if k == "q":
             exit(0)
-        elif k == "w":
+        elif k in ["w", "\x1b[A"]:
             head_pos[0] -= 1
             head_pos[0] %= map_height
-        elif k == "d":
+        elif k in ["d", '\x1b[C']:
             head_pos[1] += 1
             head_pos[1] %= map_width
-        elif k == "s":
+        elif k in ["s", '\x1b[B']:
             head_pos[0] += 1
             head_pos[0] %= map_height
-        elif k == "a":
+        elif k in ["a", '\x1b[D']:
             head_pos[1] -= 1
             head_pos[1] %= map_width
 
