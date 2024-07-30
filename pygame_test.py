b7cf31c2b0e12a7ca4a475461ac2ea82e71d64b3
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-07-29 19:16:54 -0500

    Create player in game

--- pygame_test.py
+++ pygame_test.py
@@ -6,11 +6,19 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = pygame.Rect(300, 250, 50, 50)
+
 run = True
 
 while run:
+
+    pygame.draw.rect(screen, (255, 0, 0), player)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
