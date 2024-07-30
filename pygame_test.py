7f009bb512aa321277ff3c5ae05f4172248effbb
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-07-29 19:24:04 -0500

    Player can move itself

--- pygame_test.py
+++ pygame_test.py
@@ -12,8 +12,20 @@ player = pygame.Rect(300, 250, 50, 50)
 run = True
 
 while run:
+    screen.fill((0, 0, 0))
 
     pygame.draw.rect(screen, (255, 0, 0), player)
+
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_a]:
+        player.move_ip(-1, 0)
+    elif key[pygame.K_d]:
+        player.move_ip(1, 0)
+    elif key[pygame.K_s]:
+        player.move_ip(0, 1)
+    elif key[pygame.K_w]:
+        player.move_ip(0, -1)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
